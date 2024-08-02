fa590c1edeec76ef2331a67fc04c6a2050833145
Whitney Bean <143766355+White-Lights@users.noreply.github.com>
2024-08-01 22:29:14 -0500

    Write playRound function for single game round

--- script.js
+++ script.js
@@ -20,16 +20,33 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-// Function for getting the player's choice. Corrects them
-// if player chooses anything but rock, paper, or scissors
-// and prompts a new choice.
+// Function for getting the player's choice.
 function getHumanChoice() {
     humanChoice = prompt("Make your choice! Rock, paper, or scissors?");
     humanChoice = humanChoice.toLowerCase();
     return humanChoice;
 }
 
-getHumanChoice();
-getComputerChoice();
-console.log(humanChoice + " is the player choice!");
-console.log(computerChoice + " is the computer's choice!")
+function playRound (humanChoice, computerChoice) {
+    // Take both choices, log in the console who picked what.
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+    console.log("The meatbag throws " + humanChoice + " and computer throws " + computerChoice + "!");
+    
+    // Compare the two choices and return a winner. Increment score based on victor.
+    // First, check if the both players have the same choice.
+    if (humanChoice === computerChoice) {
+        console.log("Tie! The war continues.")
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") 
+        || (humanChoice === "scissors" && computerChoice === "paper")
+        || (humanChoice === "paper" && computerChoice === "rock")) {
+            console.log("Victory for the meatbags!")
+            humanScore++;
+        } else {
+            console.log("Computer victory! The robot war will continue until morale improves.")
+            computerScore++;
+        }
+    console.log("The humans currently score " + humanScore + " and the robots score " + computerScore + ".")
+}
+
+playRound(humanChoice, computerChoice);
